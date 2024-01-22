5e88b80aa8391e216eb237767158f5512a5ddf14
Zafar <baigzafar@gmail.com>
2024-01-22 16:08:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// ==========================================================
+document.querySelector(".toggle input").addEventListener("change", (evt) => {
+  document.documentElement.dataset.mode = evt.currentTarget.checked
+    ? "dark"
+    : "light";
+});
